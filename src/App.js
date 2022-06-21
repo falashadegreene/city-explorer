a75d848039825d3a68df6e67519320f2b5cb17be
Falashade Greene <falashadegreene@gmail.com>
2022-06-21 00:58:20 -0400

    added link

--- src/App.js
+++ src/App.js
@@ -4,81 +4,87 @@ import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        value: '',
-        starWarsChars: [],
-        cityData: {},
-        error: false,
-        errorMessage: ''
-      };
-    }
-  
-   handleSubmit = async (event) => {
-     event.preventDefault();
+    this.state = {
+      city: '',
+      starWarsChars: [],
+      cityData: {},
+      error: false,
+      errorMessage: ''
+    };
+  }
 
-      try {
-     
-     let starWarsCharacters = await axios.get('https://swapi.dev/api/people/?page=1');
-      
-     this.setState({
-       starWarsChars: starWarsCharacters.data.results,
-       error: false
-     });
-  
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // make a request to the SW API and get data
+      let starWarsCharacters = await axios.get('https://swapi.dev/api/people/?page=1');
+      // proof of life
+      // console.log(starWarsCharacters.data.results);
+      // save the data in state
+      this.setState({
+        starWarsChars: starWarsCharacters.data.results,
+        error: false
+      });
     } catch (error) {
       console.log('error: ', error)
-      console.log('error.message:', error.message);
+      console.log('error.message: ', error.message);
       this.setState({
         error: true,
-        errorMessage: `An Error Occured: ${error.response.status}`
-
+        errorMessage: `An Error Occurred: ${error.response.status}`
       });
     }
-   
-   };
+  };
 
-    handleCityInput = async (e) => {
-      e.preventDefault();
+  handleCityInput = (e) => {
+    this.setState({
+      city: e.target.value
+    });
+  };
 
-      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
-      let cityInfo = await axios.get(url);
-      console.log(cityInfo.data[0]);
-    }
-  
-  
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+    // make my request to my API
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+    let cityInfo = await axios.get(url);
+    console.log(cityInfo.data[0]);
+  }
+
   render() {
+    // console.log(this.state.starWarsChars);
+    // console.log(this.state.city);
+
+    // map image src
+    // `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=47.6038321,-122.3300624&zoom=10`
 
 
-    let starWarList = this.starWarsChars.map((character, idx) => {
+    let starWarList = this.state.starWarsChars.map((character, idx) => {
       return <li key={idx}>{character.name}</li>;
     })
 
     return (
-     <>
+      <>
         <h1>Data from an API</h1>
-        <form onSubmit ={this.handleSubmit}>
-          <button type="submit">Explore!</button>
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">Display Star Wars data</button>
         </form>
         {/* WTF */}
         {this.state.error
+          // render the error message:
           ? <p>{this.state.errorMessage}</p>
-
+          // render the star wars list:
           : <ul>
             {starWarList}
           </ul>
-        
         }
-        
         <form onSubmit={this.handleCitySubmit}>
-              <label>Pick a City:
-                <input type="text" onInput={this.handleCityInput} />
-              </label>
-              <button type="submit">Get City Data</button>
-            </form>
+          <label>Pick a City:
+            <input type="text" onInput={this.handleCityInput} />
+          </label>
+          <button type="submit">Get City Data</button>
+        </form>
       </>
-  );
- }
+    );
+  }
 }
 
-  
 export default App;
